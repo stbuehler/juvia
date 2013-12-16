78c936877f23d1d2bc6a274d1a56b79b3cea9fed
Stefan Bühler <stbuehler@web.de>
2013-12-16 16:11:43 +0100

    use jquery/jquery_ujs from jquery-rails

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -1,5 +1,6 @@
 //= include api-header.js
-//= require jquery-1.7.1.min.js
+//= require jquery
+//= require jquery_ujs
 //= require jquery.delayed-observer.js
 //= require jquery.example.min.js
 // need include as it writes a "global" var, which is function local
